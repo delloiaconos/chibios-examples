f07dc9c3a4b771a480c184288f4c0fa82c5d1287
dodorega <dodorega@gmail.com>
2020-09-08 10:36:02 +0200

    -- UPDATED: main.c

--- RT-STM32F401RE-NUCLEO64-SSD1306-EX01/main.c
+++ RT-STM32F401RE-NUCLEO64-SSD1306-EX01/main.c
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2020 Domenico Rega
+    ChibiOS - Copyright (C) 2020 Domenico Rega dodorega@gmail.com
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.

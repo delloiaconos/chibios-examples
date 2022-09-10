ad924911fb994522d6d547bce50f353f41b74cf6
Salvatore Dello Iacono <delloiaconos@gmail.com>
2022-09-10 22:11:14 +0200

    -- MODIFIED GPIO05 Project

--- RT-STM32F401RE-NUCLEO64-GPIO05/main.c
+++ RT-STM32F401RE-NUCLEO64-GPIO05/main.c
@@ -1,6 +1,6 @@
 /*
-    NeaPolis Innovation Summer Campus 2021 Examples 
-    Copyright (C) 2020-2021 Salvatore Dello Iacono [delloiaconos@gmail.com]
+    NeaPolis Innovation Summer Campus Examples
+    Copyright (C) 2020-2022 Salvatore Dello Iacono [delloiaconos@gmail.com]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -16,30 +16,68 @@
 */
 
 /*
- * [GPIO05] Using GPIO Peripherals - Example 05
- * How to use the USER Button to read the user input and change the state
- * of the on board LED.
+ * [NISC2022-GPIO05] - Multithreading use of GPIO with button and LED.
+ * DESCRIPTION: How use a thread to read the user input and a thread to manage the LED.
+ * NOTE: Add an external pull-up resistor to the button; suggested R >= 4.7 kOhm
  */
 
 #include "ch.h"
 #include "hal.h"
+
+
+#define EBTN_PORT     GPIOC
+#define EBTN_PIN      7U
+#define EBTN_LINE     PAL_LINE( EBTN_PORT, EBTN_PIN )
+
+#define BLED_PORT    GPIOA
+#define BLED_PIN     7U
+#define BLED_LINE    PAL_LINE( BLED_PORT, BLED_PIN )
+
+/*
+ * This global flag variable will communicate if the button was pressed!
+ */
+static uint32_t flag = 0;
+
+/*
+ * Working Area and Thread declarations.
+ */
+static THD_WORKING_AREA( waLed, 128 );
+static THD_FUNCTION( thdLed, arg ) {
+  (void) arg;
+
+  palSetLineMode( BLED_LINE, PAL_MODE_OUTPUT_PUSHPULL );
+  while( 1 ) {
+    /* If the flag equals to 1 than:
+     * 1- the flag becomes 0 (again)
+     * 2- the led toggled
+     */
+    if( flag == 1 ) {
+      flag = 0;
+      palToggleLine( BLED_LINE );
+    }
+    chThdSleepMilliseconds( 200 );
+  }
+}
+
 
 int main(void) {
 
   halInit();
   chSysInit();
 
-  while (true) {
-      /*
-       * Read the LINE_BUTTON that refers to the USER button on the NUCLEO-64
-       * Board and if it has been pressed (condition PAL_LOW) turn on the LED.
-       */
-      if( palReadLine( LINE_BUTTON ) == PAL_LOW ) {
-        palSetLine( LINE_LED_GREEN );
-      } else {
-        palClearLine( LINE_LED_GREEN );
-      }
+  chThdCreateStatic(waLed, sizeof(waLed), NORMALPRIO + 1, thdLed, NULL );
 
-      chThdSleepMilliseconds( 25 );
+  palSetLineMode( EBTN_LINE, PAL_MODE_INPUT );
+  while (true) {
+
+    /* This loop does not set the flag variable to 0 */
+    if( palReadLine( EBTN_LINE ) == PAL_LOW ) {
+      while( palReadLine( EBTN_LINE ) == PAL_LOW ) {
+        chThdSleepMilliseconds(20);
+      }
+      flag = 1;
+    }
+
+    chThdSleepMilliseconds(20);
   }
 }

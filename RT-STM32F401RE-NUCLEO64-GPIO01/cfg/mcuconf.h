ee40e33ad52cab51896c69f3b0ebad9f4627ee04
Salvatore Dello Iacono <delloiaconos@gmail.com>
2024-09-22 19:27:10 +0200

    UPDATED Project GPIO01 to Chibios2111 & ChibiStudio 2023-02

--- RT-STM32F401RE-NUCLEO64-GPIO01/cfg/mcuconf.h
+++ RT-STM32F401RE-NUCLEO64-GPIO01/cfg/mcuconf.h
@@ -182,7 +182,7 @@
  * SERIAL driver system settings.
  */
 #define STM32_SERIAL_USE_USART1             FALSE
-#define STM32_SERIAL_USE_USART2             TRUE
+#define STM32_SERIAL_USE_USART2             FALSE
 #define STM32_SERIAL_USE_USART6             FALSE
 
 /*
